b8fded8c61ebd8438641ed3946820bcb4e4cbfe4
Jim Weiss <jweiss@motor.com>
2016-06-29 22:11:59 -0400

    Done

---
+++ Week3Day3XML/Books.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Week3Day3XML
+{
+    public class Books
+    {
+        string id;
+        string author;
+        string title;
+        string genre;
+        string price;
+        string publish_date;
+        string description;
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Genre
+        {
+            get
+            {
+                return genre;
+            }
+
+            set
+            {
+                genre = value;
+            }
+        }
+
+        public string Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public string Publish_date
+        {
+            get
+            {
+                return publish_date;
+            }
+
+            set
+            {
+                publish_date = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+    }
+}
--- Week3Day3XML/Program.cs
+++ Week3Day3XML/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Week3Day3XML
 {
@@ -10,6 +11,29 @@ namespace Week3Day3XML
     {
         static void Main(string[] args)
         {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(@"books.xml");
+
+            XmlNodeList nodes = doc.DocumentElement.SelectNodes("/catalog/book");
+
+            List<Books> books = new List<Books>();
+
+            foreach (XmlNode node in nodes)
+            {
+                Books book = new Books();
+
+                book.Id = node.Attributes["id"].Value;
+                book.Author = node.SelectSingleNode("author").InnerText;
+                book.Title = node.SelectSingleNode("title").InnerText;
+                book.Genre = node.SelectSingleNode("genre").InnerText;
+                book.Price = node.SelectSingleNode("price").InnerText;
+                book.Publish_date = node.SelectSingleNode("publish_date").InnerText;
+                book.Description = node.SelectSingleNode("description").InnerText;
+
+                books.Add(book);
+            }
+            System.Console.WriteLine("Total books: " + books.Count);
+            Console.ReadLine();
         }
     }
 }
